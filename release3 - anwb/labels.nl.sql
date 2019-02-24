d3d8e2c76e82740c57885f29f2e4ba0adecb4ac0
Bhaal22 <muller.john@gmail.com>
2019-02-24 11:05:34 +0100

    anwb labels

---
+++ release3 - anwb/labels.nl.sql
@@ -0,0 +1,203 @@
+insert_or_update_message('lblIntroTitle.Group4', 'en_GB', 'Why ANWB Parking - Basic?');
+insert_or_update_message('lblIntroTitle.Group422', 'en_GB', 'Why ANWB Parking - Short parking?');
+insert_or_update_message('lblIntroTitle.Group423', 'en_GB', 'Why ANWB Parking - Frequent parking?');
+insert_or_update_message('lblIntroList.Group4', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroList.Group422', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroList.Group423', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroText.Group4', 'en_GB', '<p>ANWB Membership benefit: Free registration and &euro; 0,28 transaction costs (instead of &euro; 0,34).</p>
+First 30 days no transaction costs!
+<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroText.Group422', 'en_GB', 'ANWB Membership benefit: Free registration and variable transaction costs from € 0,15 - € 0,40 (instead of 0,50). First 30 days no transaction costs!');
+insert_or_update_message('lblIntroText.Group423', 'en_GB', '<p>ANWB Membership benefit: Free registration and &euro; 0,55 per week (instead of &euro; 0,75). </p>
+First 30 days no subscription costs!
+<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblYouGetDiscount', 'en_GB', 'Save up to 20% of your parking costs');
+insert_or_update_message('lblNoSubscriptionRequired', 'en_GB', 'Save up to 20% of your parking costs');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group4', 'en_GB', 'With ANWB Parking - Basic you only pay &euro; 0,28 per parking transaction, on top of regular parking fees.
+<br><br/> 
+This is&nbsp;<strong>no</strong>&nbsp;subscription. You only pay per use.<strong> No usage</strong>, means <strong>no costs</strong>. 
+<br><br/> 
+For customers who park more than two times a week, there&#39;s the possibility to select a subscription for &euro; 0,55 per week. 
+Transaction costs will not be charged and parking with ANWB Parking - Frequent, parking will be cheaper.');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group422', 'en_GB', 'With ANWB parking - Short parking you only pay 10% of your parking fee (&euro; 0,15 up to a maximum of &euro; 0,40) on top of regular parking fees.
+<br><br/> 
+This is&nbsp;<strong>no</strong>&nbsp;subscription. You only pay per use.<strong> No usage</strong>, means <strong>no costs</strong>. 
+<br><br/> 
+For customers who park more than two times a week, there&#39;s the possibility to select a subscription for &euro; 0,55 per week. 
+Transaction costs will not be charged and parking with ANWB Parking - Frequent, parking will be cheaper.');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group423', 'en_GB', 'For customers who park more than two times a week, there&#39;s the posilibity to select a subscription for &euro; 0,55 per week with ANWB Parkeren - Frequent parking.
+<br><br/> 
+Transaction costs will not be charged and parking with ANWB Parking - Frequent, parking will be cheaper.');
+insert_or_update_message('lblIntroTablet.Group4', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroTablet.Group422', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('lblIntroTablet.Group423', 'en_GB', '<ul>
+<li>With the reliable ANWB Onderweg parking app and through 088-2742536.</li>
+<li>Free parking reminders in the ANWB Onderweg app.</li>
+<li>Monthly invoice and pay afterwards for the real parking time.</li>
+<li>Easy parking in over 160 cities and over 150 Q-park locations.</li>
+</ul>');
+insert_or_update_message('Registration.EmailBody.Business.Group4', 'en_GB', '<p><div class="p-light-blue">Thanks for your registration at ANWB Parkeren. ANWB Parkeren is carried out in collaboration with Yellowbrick.</div></p>
+<br/>
+Your registration is currently being processed by Yellowbrick. You&#39;ll receive an e-mail with your customer number and a link to create a password to login into the ANWB Onderweg App and start with mobile parking.
+<br/> 
+<p>Have you ordered cards? These will be send to you by mail so you will receive them within a few days.</p>
+<br/>
+<p><strong>Welcome gift: New customers of ANWB parking get the first 30 days without transaction fee. Your 30 days are active today.</strong></p>
+<br/>
+<p>Kind regards,</p>
+ANWB Parking
+<p>(Powered by Yellowbrick)</p>');
+insert_or_update_message('Registration.EmailBody.Private.Group4', 'en_GB', '<p><div class="p-light-blue">Thanks for your registration at ANWB Parkeren. ANWB Parkeren is carried out in collaboration with Yellowbrick.</div></p>
+<br/>
+Your registration is currently being processed by Yellowbrick. You&#39;ll receive an e-mail with your customer number and a link to create a password to login into the ANWB Onderweg App and start with mobile parking.
+<br/> 
+<p>Have you ordered cards? These will be send to you by mail so you will receive them within a few days.</p>
+<br/>
+<p><strong>Welcome gift: New customers of ANWB parking get the first 30 days without transaction fee. Your 30 days are active today.</strong></p>
+<br/>
+<p>Kind regards,</p>
+ANWB Parking
+<p>(Powered by Yellowbrick)</p>');
+insert_or_update_message('twitterLink.Group4', 'en_GB', 'https://twitter.com/ANWB');
+insert_or_update_message('twitterLink.Group422', 'en_GB', 'https://twitter.com/ANWB');
+insert_or_update_message('twitterLink.Group423', 'en_GB', 'https://twitter.com/ANWB');
+
+
+insert_or_update_message('lblIntroTitle.Group4', 'nl_NL', 'Hierom ANWB Parkeren – Standaard');
+insert_or_update_message('lblIntroTitle.Group422', 'nl_NL', 'Hierom ANWB Parkeren – Kort parkeren');
+insert_or_update_message('lblIntroTitle.Group423', 'nl_NL', 'Hierom ANWB Parkeren – Vaak parkeren');
+insert_or_update_message('lblIntroList.Group4', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblIntroList.Group422', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblIntroList.Group423', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblIntroText.Group4', 'nl_NL', '<p>ANWB Ledenvoordeel: Gratis registratie en &euro; 0,28 transactiekosten (i.p.v. &euro; 0,34).</p>
+Eerste 30 dagen geen transactiekosten!
+<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>
+');
+insert_or_update_message('lblIntroText.Group422', 'nl_NL', 'ANWB Ledenvoordeel: Gratis registratie en variabele transactiekosten van € 0,15 -  € 0,40 (i.p.v. € 0,50). Eerste 30 dagen geen transactiekosten!');
+insert_or_update_message('lblIntroText.Group423', 'nl_NL', '<p>ANWB Ledenvoordeel: Gratis registratie en &euro; 0,55 per week (i.p.v. &euro; 0,75).</p>
+Eerste 30 dagen geen transactiekosten!
+<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblYouGetDiscount', 'nl_NL', 'Bespaar tot 20% op je parkeerkosten!');
+insert_or_update_message('lblNoSubscriptionRequired', 'nl_NL', 'Bespaar tot 20% op je parkeerkosten!');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group4', 'nl_NL', 'Bij ANWB Parkeren - Standaard betaal je slechts &euro; 0,28 per transactie, bovenop de gemeentelijke parkeerbelasting.
+<br><br/> 
+Het is&nbsp;<strong>geen</strong>&nbsp;abonnement. <strong>Parkeer je niet, dan betaal je ook</strong>&nbsp;<strong>niets</strong>. 
+<br><br/> 
+Voor klanten die meer dan 2 keer per week parkeren is er wel de mogelijkeid om een abonnement van &euro; 0,55 cent per week in te stellen. 
+Dan vervallen de transactiekosten en is parkeren met ANWB Parkeren - Vaak, parkeren goedkoper.');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group422', 'nl_NL', 'Bij ANWB Parkeren – Kort parkeren betaal je slechts 10&#37; van je parkeerbelasting (&euro; 0,15 tot maximaal &euro; 0,40) als transactiekosten, bovenop de gemeentelijke parkeerbelasting
+<br><br/> 
+Het is&nbsp;<strong>geen</strong>&nbsp;abonnement. <strong>Parkeer je niet, dan betaal je ook</strong>&nbsp;<strong>niets</strong>. 
+<br><br/> 
+Voor klanten die meer dan 2 keer per week parkeren is er wel de mogelijkeid om een abonnement van &euro; 0,55 cent per week in te stellen. 
+Dan vervallen de transactiekosten en is parkeren met ANWB Parkeren - Vaak, parkeren goedkoper.');
+insert_or_update_message('lblNoSubscriptionRequiredText.Group423', 'nl_NL', 'Bij ANWB Parkeren - Vaak parkeren heb je een abonnement en betaal je slechts &euro; 0,55 per week ongeacht hoe vaak je parkeert. 
+<br><br/>
+Je betaalt geen transactiekosten per parkeertransactie. 
+<br><br/>
+Parkeer je minder dan 2 keer per week dan is ANWB Parkeren - standaard waar je &euro; 0,28 per transactie betaalt goedkoper.');
+insert_or_update_message('lblIntroTablet.Group4', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblIntroTablet.Group422', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('lblIntroTablet.Group423', 'nl_NL', '<ul>
+<li>Werkt met de betrouwbare ANWB Onderweg parkeerapp en via 088-2742536.</li>
+<li>Gratis parkeerherinneringen in ANWB Onderweg app.</li>
+<li>Maandelijkse factuur en alleen achteraf betalen voor de werkelijke parkeertijd.</li>
+<li>Gemakkelijk parkeren in meer dan 160 steden en ruim 150 Q-Park garages en terreinen.</li>
+</ul>');
+insert_or_update_message('Registration.EmailBody.Business.Group4', 'nl_NL', '<p><div class="p-light-blue">Bedankt voor uw registratie bij ANWB Parkeren. ANWB Parkeren wordt uitgevoerd in samenwerking met Yellowbrick.</div></p>
+<br />
+<p>Uw registratie wordt momenteel door Yellowbrick verwerkt. U ontvangt straks een volgende email met uw klantnummer en een link waarmee u uw wachtwoord kunt aanmaken om in te loggen in de ANWB Onderweg app en kunt starten met mobiel parkeren.</p>
+<br/>
+<p>Heeft u P+ passen besteld voor Q-Park? Deze ontvangt u binnen 5 werkdagen op het opgegeven adres.</p>
+<br />
+<p><strong>Welkomstcadeau: nieuwe klanten van ANWB Parkeren betalen in de eerste 30 dagen geen transactiekosten. Uw 30 dagen gaan vandaag in.</strong></p>
+<br/>
+Met vriendelijke groet,<br />
+<p>ANWB Parkeren</p>
+<p>(In samenwerking met Yellowbrick)</p>');
+insert_or_update_message('Registration.EmailBody.Private.Group4', 'nl_NL', '<p><div class="p-light-blue">Bedankt voor uw registratie bij ANWB Parkeren. ANWB Parkeren wordt uitgevoerd in samenwerking met Yellowbrick.</div></p>
+<br />
+<p>Uw registratie wordt momenteel door Yellowbrick verwerkt. U ontvangt straks een volgende email met uw klantnummer en een link waarmee u uw wachtwoord kunt aanmaken om in te loggen in de ANWB Onderweg app en kunt starten met mobiel parkeren.</p>
+<br/>
+<p>Heeft u P+ passen besteld voor Q-Park? Deze ontvangt u binnen 5 werkdagen op het opgegeven adres.</p>
+<br />
+<p><strong>Welkomstcadeau: nieuwe klanten van ANWB Parkeren betalen in de eerste 30 dagen geen transactiekosten. Uw 30 dagen gaan vandaag in.</strong></p>
+<br/>
+Met vriendelijke groet,<br />
+<p>ANWB Parkeren</p>
+<p>(In samenwerking met Yellowbrick)</p>');
+insert_or_update_message('twitterLink.Group4', 'nl_NL', 'https://twitter.com/ANWB');
+insert_or_update_message('twitterLink.Group422', 'nl_NL', 'https://twitter.com/ANWB');
+insert_or_update_message('twitterLink.Group423', 'nl_NL', 'https://twitter.com/ANWB');
+
+
